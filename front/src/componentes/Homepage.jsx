3b360e47345ee3ab6244c67f57a334c6789b640a
ricardo padilha <padilha.ricardo@hotmail.com>
2018-12-11 12:30:19 -0200

    teste com componentdidmount

--- front/src/componentes/Homepage.jsx
+++ front/src/componentes/Homepage.jsx
@@ -3,41 +3,37 @@ import React from 'react';
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
-    this.fetchDB = this.fetchDB.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
     this.state = {
-      id: "",
-      nome: "",
-      usuario: "",
-      qtdJogos: ""
+      infos: ""
     }
   }
-  fetchDB(e) {
+  componentDidMount(e) {
     fetch('http://localhost/loja-de-jogos/api/usuarios/48', {
       method: 'GET',
     })
     .then(response => response.json())
-    .then(response => {
-      this.setState({ [response.id] : response.value })
+    .then(infos => {
+      this.setState({ infos })
     });
   }
 
   render() {
+    const { infos } = this.state;
     return (
       <div>
         <p>
-          <label>Id: {this.state.id}</label>
+          <label>Id: { infos.id }</label>
         </p>
         <p>
-          <label>Nome: {this.state.nome}</label>
+          <label>Nome: { infos.nome }</label>
         </p>
         <p>
-          <label>Usuario: {this.state.usuario}</label>
+          <label>Usuario: { infos.usuario }</label>
         </p>
         <p>
-          <label>Jogos: {this.state.qtdJogos}</label>
+          <label>Jogos: { infos.qtdJogo }</label>
         </p>
-
-        <button onClick={this.fetchDB}>Carregar usuario</button>
       </div>
     );
   }
